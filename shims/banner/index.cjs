5eaef8c58fe1b0d6022333bada1668dc371d3d77
Artur Sapek <art@wormholelabs.xyz>
2024-05-07 12:16:16 -0400

    apply patch to 0.17

--- shims/banner/index.cjs
+++ shims/banner/index.cjs
@@ -1,4 +1,5 @@
 // eslint-disable-next-line n/prefer-global/buffer
+globalThis = globalThis ?? window
 globalThis.Buffer = globalThis.Buffer || Buffer
 globalThis.global = globalThis.global || global
 globalThis.process = globalThis.process || process
